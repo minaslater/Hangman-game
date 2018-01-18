ddaa56bf0838900cb8e628a46866ef2fba0c00f1
Mina Slater <slater.mina@gmail.com>
2018-01-18 16:06:35 -0600

    adds random word selection functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,8 @@
-alert("linked!");
+var generateWord = function() {
+  var availableWords = ["dragonfruit", "papayas", "pear", "pineapple", "cranberries"];
+  var randomIndex = Math.floor(Math.random() * availableWords.length);
+  return availableWords[randomIndex];
+}
+
+var wordToSolve = generateWord();
+console.log(wordToSolve);
